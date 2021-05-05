d6471a1926aea7615388194db3ec2d20708ec9fe
Nare <nare.zakaryanmm@gmail.com>
2021-05-05 21:26:10 +0400

    example for moving block exercises to start

---
+++ js/movingBlock.js
@@ -0,0 +1,11 @@
+let objBlock = null;
+
+function init(){
+  objBlock = document.getElementsByClassName("block")[0];
+  objBlock.style.left = '0px';
+  objBlock.style.top = '0px';
+}
+
+// script goes here
+
+window.onload = init;
